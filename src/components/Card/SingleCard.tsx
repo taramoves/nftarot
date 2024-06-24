c6cbb273aec122b93183aca58b0d65992dbb4c26
jennaleonard <jleonardweb@gmail.com>
2024-06-24 16:24:12 -0700

    imports cont.

--- src/components/Card/SingleCard.tsx
+++ src/components/Card/SingleCard.tsx
@@ -1,4 +1,4 @@
-import styles from "../../styles/Card.module.css";
+import styles from "../../styles/CardSelect.module.css";
 import Image, { StaticImageData } from "next/image";
 
 interface Props {
