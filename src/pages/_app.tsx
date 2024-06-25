aa3671556819d9d90d7e7bc90d8ccecd2b14f691
jennaleonard <jleonardweb@gmail.com>
2024-06-25 11:38:35 -0700

    added privy - stable

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { customTheme } from "@/theme";
 import { fonts } from "@/lib/fonts";
+import {PrivyProvider} from '@privy-io/react-auth';
+
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -13,9 +15,25 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           }
         `}
       </style>
+      <PrivyProvider
+      appId="clx70rx9j01ohqehtnicm2ku1"
+      config={{
+        // Customize Privy's appearance in your app
+        appearance: {
+          theme: 'light',
+          accentColor: '#676FFF',
+          logo: 'https://your-logo-url',
+        },
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          createOnLogin: 'users-without-wallets',
+        },
+      }}
+    >
       <ChakraProvider resetCSS theme={customTheme}>
         <Component {...pageProps} />
       </ChakraProvider>
+      </PrivyProvider>
     </>
   );
 }
