d4625c44111d6ad4f63624e5a56f91a3f4466549
jennaleonard <jleonardweb@gmail.com>
2024-07-26 15:08:59 -0700

    deleted unused files, swiper library, and irrelevant stories components. Updated CardCarousel to be reponsive and position cards on an angle, fixed issue with re-rendering

--- src/pages/slide/index.tsx
+++ src/pages/slide/index.tsx
@@ -1,6 +1,6 @@
 import NavBar from "@/components/NavBar";
 import Page from "@/components/Page";
-import { Flex } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import CardCarousel from "@/components/Card/CardCarousel";
 
 //temporary page for testing out the slider view
@@ -8,16 +8,14 @@ export default function SliderView() {
   return (
     <Page variant={"main"}>
       <NavBar />
-      <Flex
+      <Box
         style={{
           width: "100%",
-          alignItems: "center",
-          justifyContent: "center",
           margin: "30% auto",
         }}
       >
         <CardCarousel />
-      </Flex>
+      </Box>
     </Page>
   );
 }
