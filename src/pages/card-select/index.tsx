f4b5f95c3d4dd5cde018d5fbed529c1f1cb273ca
jennaleonard <jleonardweb@gmail.com>
2024-08-21 12:47:14 -0700

    updated minting function

--- src/pages/card-select/index.tsx
+++ src/pages/card-select/index.tsx
@@ -11,10 +11,10 @@ import { createReading, createOrUpdateUser } from "@/utils/dbUtils";
 import { usePrivy } from "@privy-io/react-auth";
 import usePrivyWalletClient from "@/hooks/usePrivyWalletClient";
 import { base } from "viem/chains";
-import { zoraCreator1155ImplABI } from '@zoralabs/protocol-deployments';
+import { zoraCreator1155ImplABI } from "@zoralabs/protocol-deployments";
 import { Address, encodeAbiParameters, parseAbiParameters } from "viem";
 import useConnectedWallet from "@/hooks/useConnectedWallet";
-import useNetworkCheck from '@/hooks/useNetworkCheck'; // New import
+import useNetworkCheck from "@/hooks/useNetworkCheck"; // New import
 
 interface CardState {
   fileName: string;
@@ -46,7 +46,7 @@ export default function CardSelect() {
       return Math.min(baseRadius, maxRadius);
     };
     let radius = calculateRadius();
-  
+
     const calculatePositions = () => {
       const angleStep = (2 * Math.PI) / numCards;
       return Array.from({ length: numCards }, (_, i) => {
@@ -56,17 +56,17 @@ export default function CardSelect() {
         return { x, y, angle };
       });
     };
-  
+
     setPositions(calculatePositions());
-  
+
     const handleResize = () => {
       radius = calculateRadius();
       setPositions(calculatePositions());
     };
-  
-    window.addEventListener('resize', handleResize);
-  
-    return () => window.removeEventListener('resize', handleResize);
+
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   const handleCardClick = (cardIndex: number) => {
@@ -104,32 +104,42 @@ export default function CardSelect() {
       const randomIndex = generateRandomIndex();
       console.log("Generated random index:", randomIndex);
 
-      const minter = '0x04E2516A2c207E84a1839755675dfd8eF6302F0a' as Address; //'0xd34872BE0cdb6b09d45FCa067B07f04a1A9aE1aE' as Address;
+      const minter = "0x04E2516A2c207E84a1839755675dfd8eF6302F0a" as Address; //'0xd34872BE0cdb6b09d45FCa067B07f04a1A9aE1aE' as Address;
       const tokenId = BigInt(randomIndex + 1);
       const quantity = BigInt(1);
-      const minterArguments = encodeAbiParameters(parseAbiParameters('address x, string y'), [
-        connectedWallet as Address,
-        'received a tarot reading onchain',
-      ]);    
-      const mintReferral = '0xD246C16EC3b555234630Ab83883aAAcdfd946ceF' as Address;
-      const args = [minter, tokenId, quantity, minterArguments, mintReferral];
-    
+      const rewardsRecipients = [
+        "0xD246C16EC3b555234630Ab83883aAAcdfd946ceF" as Address,
+        "0xD246C16EC3b555234630Ab83883aAAcdfd946ceF" as Address,
+      ];
+      const minterArguments = encodeAbiParameters(
+        parseAbiParameters("address x, string y"),
+        [connectedWallet as Address, "received a tarot reading onchain"]
+      );
+
+      const args = [
+        minter,
+        tokenId,
+        quantity,
+        rewardsRecipients,
+        minterArguments,
+      ];
+
       console.log(args);
       await (walletClient as any).writeContract({
-        address: '0x67D7e7BCc964De5BEf0951EB818E3A9A136312B5', //baseSepolia: '0x4B713bC4CEC525E59f5E6D1Cd3a372D0ee747E6d',
+        address: "0x67D7e7BCc964De5BEf0951EB818E3A9A136312B5", //baseSepolia: '0x4B713bC4CEC525E59f5E6D1Cd3a372D0ee747E6d',
         abi: zoraCreator1155ImplABI,
-        functionName: 'mintWithRewards',
+        functionName: "mint",
         account: connectedWallet as Address,
         args: args as any,
-        value: '777000000000000'
+        value: "777000000000000",
       } as any);
 
       console.log("Minting process completed");
-      
-      const deckId = 'd8a4f60f-f3bf-44df-9218-7a10e4dfdf46';
-      
+
+      const deckId = "d8a4f60f-f3bf-44df-9218-7a10e4dfdf46";
+
       const card = await getCardByIndex(deckId, randomIndex);
-      
+
       if (!card) {
         throw new Error("Failed to get a card at the generated index");
       }
@@ -138,13 +148,13 @@ export default function CardSelect() {
 
       if (user?.wallet?.address) {
         await createOrUpdateUser(user.wallet.address);
-        
+
         const card = await getCardByIndex(deckId, randomIndex);
-      
-      if (!card) {
-        throw new Error("Failed to get a card at the generated index");
-      }
-      
+
+        if (!card) {
+          throw new Error("Failed to get a card at the generated index");
+        }
+
         // Create reading in the database
         const readingId = await createReading(
           user.wallet.address,
@@ -152,12 +162,12 @@ export default function CardSelect() {
           deckId,
           card.image_url,
           randomIndex
-        );  
+        );
         // Navigate to the card-reveal page with card data
 
         router.push({
           pathname: "/card-reveal/[readingId]",
-          query: { readingId: readingId }
+          query: { readingId: readingId },
         } as any);
       } else {
         throw new Error("Wallet address not available");
@@ -174,7 +184,8 @@ export default function CardSelect() {
       console.error("Error in minting process:", error);
       toast({
         title: "Minting Failed",
-        description: "There was an error while minting your card. Please try again.",
+        description:
+          "There was an error while minting your card. Please try again.",
         status: "error",
         duration: 5000,
         isClosable: true,
@@ -191,7 +202,7 @@ export default function CardSelect() {
       </Head>
       <Navbar />
       <div className={styles.main}>
-      {/* <div
+        {/* <div
   style={{
     position: "absolute",
     borderRadius: "50%",
@@ -254,8 +265,8 @@ export default function CardSelect() {
                   Please switch to the correct network to mint.
                 </Box>
               )}
-              <Button 
-                variant={"primaryButton"} 
+              <Button
+                variant={"primaryButton"}
                 onClick={handleMintClick}
                 isLoading={isMinting}
                 loadingText="Minting"
